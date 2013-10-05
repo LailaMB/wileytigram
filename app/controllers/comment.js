ddaad769c93e4f17ad88440dc16db61cf8c12738
Chaim Krause <chaim@chaim.com>
2013-10-05 12:36:49 -0500

    Mostly working. The issues that remain I believe are not accounted for in the codebase.

--- app/controllers/comment.js
+++ app/controllers/comment.js
@@ -5,8 +5,13 @@ var parentController = parameters.parentController || {};
 var comments = Alloy.Collections.instance("Comment");
 
 OS_IOS && $.newCommentButton.addEventListener("click", handleNewCommentButtonClicked);
+$.commentTable.addEventListener("delete", handleDeleteRow);
+$.commentTable.addEventListener("longpress", handleDeleteRow);
+$.commentTable.editable = true;
 
 function loadComments(_photo_id) {
+    // creates or gets the global instance of comment collection
+    var comments = Alloy.Collections.instance("Comment");
     var params = {
         photo_id : currentPhoto.id,
         order : '-created_at',
@@ -30,10 +35,94 @@ function loadComments(_photo_id) {
         }
     });
 
-}
+};
 
 function handleNewCommentButtonClicked(_event) {
-    // FILLED OUT LATER IN CHAPTER
+
+    var inputController = Alloy.createController("commentInput", {
+        photo : currentPhoto,
+        parentController : $,
+        callback : inputCallback
+    });
+
+    // open the window, modally on IOS
+    inputController.getView().open({
+        modal : OS_ANDROID ? false : true
+    });
+
+};
+
+function inputCallback(_event) {
+    if (_event.success) {
+        addComment(_event.content);
+    } else {
+        alert("No Comment Added");
+    }
+};
+
+function addComment(_content) {
+    var comment = Alloy.createModel('Comment');
+    var params = {
+        photo_id : currentPhoto.id,
+        content : _content,
+        allow_duplicate : 1
+    };
+
+    comment.save(params, {
+        success : function(_model, _response) {
+            Ti.API.info('success: ' + _model.toJSON());
+            var row = Alloy.createController("commentRow", _model);
+
+            // add the controller view, which is a row to the table
+            if ($.commentTable.getData().length === 0) {
+                $.commentTable.setData([]);
+                $.commentTable.appendRow(row.getView(), true);
+            } else {
+                $.commentTable.insertRowBefore(0, row.getView(), true);
+            }
+        },
+        error : function(e) {
+            Ti.API.error('error: ' + e.message);
+            alert('Error saving new comment ' + e.message);
+        }
+    });
+};
+
+function handleDeleteRow(_event) {
+    // _event.row.comment_id
+    var collection = Alloy.Collections.instance("Comment");
+    var model = collection.get(_event.row.comment_id);
+
+    if (!model) {
+        alert("Could not find selected comment");
+        return;
+    } else {
+        var optionAlert = Titanium.UI.createAlertDialog({
+            title : 'Alert',
+            message : 'Are You Sure You Want to Delete the Comment',
+            buttonNames : ['Yes', 'No']
+        });
+
+        optionAlert.addEventListener('click', function(e) {
+            if (e.index == 0) {
+                model.destroy({
+                    data : {
+                        photo_id : currentPhoto.id, // comment on
+                        id : model.id // id of the comment object
+                    },
+                    success : function(_model, _response) {
+                        loadComments(null);
+                    },
+                    error : function(_e) {
+                        Ti.API.error('error: ' + _e.message);
+                        alert("Error deleteing comment");
+                        loadComments(null);
+                    }
+                });
+            }
+        });
+        optionAlert.show();
+    }
 }
 
 function doOpen() {
@@ -70,5 +159,4 @@ function doOpen() {
 $.initialize = function() {
     loadComments();
 };
-
 
