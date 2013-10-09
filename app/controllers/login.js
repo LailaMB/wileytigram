b13015502b40b8d662b3e11d1d856258e4f68e92
Chaim Krause <chaim@chaim.com>
2013-10-09 08:59:11 -0500

    Adds Facebook functionality

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,8 +1,12 @@
 var args = arguments[0] || {};
 
+// Using FB module in the latest release of Appcelerator
+Alloy.Globals.FB = require('facebook');
+
 // need access to the parent object
 $.parentController = args.parentController;
 
+// events
 $.showLoginBtn.addEventListener('click', showLoginBtnClicked);
 $.showCreateAccountBtn.addEventListener('click', showCreateAccountBtnClicked);
 $.cancelCreateAcctBtn.addEventListener('click', cancelActionButtonClicked);
@@ -11,73 +15,170 @@ $.cancelLoginBtn.addEventListener('click', cancelActionButtonClicked);
 $.doLoginBtn.addEventListener('click', doLoginBtnClicked);
 $.doCreateAcctBtn.addEventListener('click', doCreateAcctBtnClicked);
 
+$.showLoginFBBtn.addEventListener('click', doFacebookLoginAction);
+// EVENT HANDLERS
+/**
+ *
+ */
 function showLoginBtnClicked() {
-    $.create_acct_view.hide();
-    $.home_view.hide();
-    $.login_view.show();
+	$.create_acct_view.hide();
+	$.home_view.hide();
+	$.login_view.show();
 };
 
+/**
+ *
+ */
 function showCreateAccountBtnClicked() {
-    $.create_acct_view.show();
-    $.home_view.hide();
-    $.login_view.hide();
+	$.create_acct_view.show();
+	$.home_view.hide();
+	$.login_view.hide();
 };
 
+/**
+ *
+ */
 function cancelActionButtonClicked() {
-    $.create_acct_view.hide();
-    $.login_view.hide();
+	$.create_acct_view.hide();
+	$.login_view.hide();
 
-    // set the global login state to false
-    Alloy.Globals.loggedIn = false;
+	// set the global login state to false
+	Alloy.Globals.loggedIn = false;
 
-    // display only the home state view
-    $.home_view.show();
+	// display only the home state view
+	$.home_view.show();
 };
 
 function userActionResponseHandler(_resp) {
-    if (_resp.success === true) {
+	if (_resp.success === true) {
 
-        // Do stuff after successful login.
-        Alloy.Globals.loggedIn = true;
-        Alloy.Globals.CURRENT_USER = _resp.model;
+		// Do stuff after successful login.
+		Alloy.Globals.loggedIn = true;
+		Alloy.Globals.CURRENT_USER = _resp.model;
 
-        $.parentController.loginSuccessAction(_resp);
+		$.parentController.loginSuccessAction(_resp);
 
-    } else {
-        // Show the error message and let the user try again.
-        alert("loginFailed", _resp.error.message);
+	} else {
+		// Show the error message and let the user try again.
+		alert("loginFailed", _resp.error.message);
 
-        Alloy.Globals.CURRENT_USER = null;
-        Alloy.Globals.loggedIn = false;
-    }
+		Alloy.Globals.CURRENT_USER = null;
+		Alloy.Globals.loggedIn = false;
+	}
 };
 
+/**
+ *
+ */
 function doLoginBtnClicked() {
 
-    var user = Alloy.createModel('User');
+	var user = Alloy.createModel('User');
 
-    user.login($.email.value, $.password.value, userActionResponseHandler);
+	user.login($.email.value, $.password.value, userActionResponseHandler);
 };
 
 function doCreateAcctBtnClicked() {
-    if ($.acct_password.value !== $.acct_password_confirmation.value) {
-        alert("Please re-enter information");
-        return;
-    }
+	if ($.acct_password.value !== $.acct_password_confirmation.value) {
+		alert("Please re-enter information");
+		return;
+	}
 
-    var params = {
-        first_name : $.acct_fname.value,
-        last_name : $.acct_lname.value,
-        username : $.acct_email.value,
-        email : $.acct_email.value,
-        password : $.acct_password.value,
-        password_confirmation : $.acct_password_confirmation.value,
-    };
+	var params = {
+		first_name : $.acct_fname.value,
+		last_name : $.acct_lname.value,
+		username : $.acct_email.value,
+		email : $.acct_email.value,
+		password : $.acct_password.value,
+		password_confirmation : $.acct_password_confirmation.value,
+	};
 
-    var user = Alloy.createModel('User');
+	var user = Alloy.createModel('User');
 
-    user.createAccount(params, userActionResponseHandler);
+	user.createAccount(params, userActionResponseHandler);
 };
+
+/**
+ *
+ * @param {Object} _event
+ */
+function faceBookLoginEventHandler(_event) {
+
+	Alloy.Globals.FB.removeEventListener('login', faceBookLoginEventHandler);
+
+	if (_event.success) {
+		doFacebookLoginAction(_event.data);
+	} else if (_event.error) {
+		alert(_event.error);
+	} else {
+		_event.cancelled && alert("User Canceled");
+	}
+};
+
+function faceBookLoginErrorHandler(_user, _error) {
+	// Show the error message somewhere and let the user try again.
+	alert("Error: " + _error.code + " " + _error.message);
+
+	Alloy.Globals.loggedIn = false;
+	Alloy.Globals.CURRENT_USER = null;
+};
+
+/**
+ *
+ * @param {Object} _options data from FB login
+ */
+function doFacebookLoginAction(_options) {
+	var FB = Alloy.Globals.FB;
+
+	if (FB.loggedIn === false) {
+
+		FB.logout();
+
+		/// Enabling single sign on using FB
+		FB.forceDialogAuth = false;
+
+		// get the app id
+		FB.appid = Ti.App.Properties.getString("ti.facebook.appid");
+
+		// set permissions
+		FB.permissions = ["read_stream"];
+
+		// login handler with callback
+		FB.addEventListener("login", faceBookLoginEventHandler);
+
+		// attempt to authorize user
+		FB.authorize();
+
+	} else {
+		var user = Alloy.createModel('User');
+		user.updateFacebookLoginStatus(FB.accessToken, {
+			success : function(_resp) {
+
+				Ti.App.Properties.setString("loginType", "FACEBOOK");
+
+				Alloy.Globals.loggedIn = true;
+				Alloy.Globals.CURRENT_USER = _resp.model;
+
+				// save the newly created facebook user
+				if (!_resp.model.get("username") && _options.email) {
+					_resp.model.save({
+						"email" : _options.email,
+						"username" : _options.email
+					}, {
+						success : function(_user, _response) {
+							$.parentController.loginSuccessAction(_resp);
+
+							Alloy.Globals.CURRENT_USER = _user;
+						},
+						error : faceBookLoginErrorHandler
+					});
+				} else {
+					$.parentController.loginSuccessAction(_resp);
+				}
+			},
+			error : faceBookLoginErrorHandler
+		});
+	}
+}
 
 /**
  * if reset is true then set the UI back to option page
@@ -85,16 +186,16 @@ function doCreateAcctBtnClicked() {
  */
 $.open = function(_reset) {
 
-    // reset the UI
-    if (_reset) {
-        cancelActionButtonClicked();
-    }
-    $.index.open();
+	// reset the UI
+	if (_reset) {
+		cancelActionButtonClicked();
+	}
+	$.index.open();
 };
 /**
  * close the window/controller, do some clean up here
  * if needed
  */
 $.close = function() { debugger;
-    $.index.close();
+	$.index.close();
 };
--- app/models/user.js
+++ app/models/user.js
@@ -1,149 +1,192 @@
 exports.definition = {
 
-    config : {
+	config : {
 
-        "adapter" : {
-            "type" : "acs",
-            "collection_name" : "users"
-        }
-    },
+		"adapter" : {
+			"type" : "acs",
+			"collection_name" : "users"
+		}
+	},
 
-    extendModel : function(Model) {
-        _.extend(Model.prototype, {
+	extendModel : function(Model) {
+		_.extend(Model.prototype, {
 
-            // extended functions go here
-            login : function(_login, _password, _callback) {
-                var TAP = Ti.App.Properties;
-                this.config.Cloud.Users.login({
-                    login : _login,
-                    password : _password
-                }, function(e) {
-                    if (e.success) {
-                        var user = e.users[0];
-                        TAP.setString("sessionId", e.meta.session_id);
-                        TAP.setString("user", JSON.stringify(user));
-                        _callback && _callback({
-                            success : true,
-                            model : new model(user)
-                        });
-                    } else {
-                        Ti.API.error(e);
-                        _callback && _callback({
-                            success : false,
-                            model : null,
-                            error : e
-                        });
-                    }
-                });
-            },
-            createAccount : function(_userInfo, _callback) {
-                var cloud = this.config.Cloud;
-                var TAP = Ti.App.Properties;
+			// extended functions go here
+			login : function(_login, _password, _callback) {
+				var TAP = Ti.App.Properties;
+				this.config.Cloud.Users.login({
+					login : _login,
+					password : _password
+				}, function(e) {
+					if (e.success) {
+						var user = e.users[0];
+						TAP.setString("sessionId", e.meta.session_id);
+						TAP.setString("user", JSON.stringify(user));
+						_callback && _callback({
+							success : true,
+							model : new model(user)
+						});
+					} else {
+						Ti.API.error(e);
+						_callback && _callback({
+							success : false,
+							model : null,
+							error : e
+						});
+					}
+				});
+			},
+			createAccount : function(_userInfo, _callback) {
+				var cloud = this.config.Cloud;
+				var TAP = Ti.App.Properties;
 
-                // bad data so return to caller
-                if (!_userInfo) {
-                    _callback && _callback({
-                        success : false,
-                        model : null
-                    });
-                } else {
-                    cloud.Users.create(_userInfo, function(e) {
-                        if (e.success) {
-                            var user = e.users[0];
-                            TAP.setString("sessionId", e.meta.session_id);
-                            TAP.setString("user", JSON.stringify(user));
+				// bad data so return to caller
+				if (!_userInfo) {
+					_callback && _callback({
+						success : false,
+						model : null
+					});
+				} else {
+					cloud.Users.create(_userInfo, function(e) {
+						if (e.success) {
+							var user = e.users[0];
+							TAP.setString("sessionId", e.meta.session_id);
+							TAP.setString("user", JSON.stringify(user));
 
-                            // set this for ACS to track session connected
-                            cloud.sessionId = e.meta.session_id;
+							// set this for ACS to track session connected
+							cloud.sessionId = e.meta.session_id;
 
-                            // callback with newly created user
-                            _callback && _callback({
-                                success : true,
-                                model : new model(user)
-                            });
-                        } else {
-                            Ti.API.error(e);
-                            _callback && _callback({
-                                success : false,
-                                model : null,
-                                error : e
-                            });
-                        }
-                    });
-                }
-            },
-            logout : function(_callback) {
-                var cloud = this.config.Cloud;
-                var TAP = Ti.App.Properties;
+							// callback with newly created user
+							_callback && _callback({
+								success : true,
+								model : new model(user)
+							});
+						} else {
+							Ti.API.error(e);
+							_callback && _callback({
+								success : false,
+								model : null,
+								error : e
+							});
+						}
+					});
+				}
+			},
+			logout : function(_callback) {
+				var cloud = this.config.Cloud;
+				var TAP = Ti.App.Properties;
 
-                cloud.Users.logout(_userInfo, function(e) {
-                    if (e.success) {
-                        var user = e.users[0];
-                        TAP.removeProperty("sessionId");
-                        TAP.removeProperty("user");
+				cloud.Users.logout(_userInfo, function(e) {
+					if (e.success) {
+						var user = e.users[0];
+						TAP.removeProperty("sessionId");
+						TAP.removeProperty("user");
 
-                        // callback clearing out the user model
-                        _callback && _callback({
-                            success : true,
-                            model : null
-                        });
-                    } else {
-                        Ti.API.error(e);
-                        _callback && _callback({
-                            success : false,
-                            model : null,
-                            error : e
-                        });
-                    }
-                });
-            },
-            authenticated : function() {
-                var cloud = this.config.Cloud;
-                var TAP = Ti.App.Properties;
+						// callback clearing out the user model
+						_callback && _callback({
+							success : true,
+							model : null
+						});
+					} else {
+						Ti.API.error(e);
+						_callback && _callback({
+							success : false,
+							model : null,
+							error : e
+						});
+					}
+				});
+			},
+			authenticated : function() {
+				var cloud = this.config.Cloud;
+				var TAP = Ti.App.Properties;
 
-                if (TAP.hasProperty("sessionId")) {
-                    Ti.API.info("SESSION ID " + TAP.getString("sessionId"));
-                    cloud.sessionId = TAP.getString("sessionId");
-                    return true;
-                }
-                return false;
-            },
-            showMe : function(_callback) {
-                var cloud = this.config.Cloud;
-                var TAP = Ti.App.Properties;
-                cloud.Users.showMe(function(e) {
-                    if (e.success) {
-                        var user = e.users[0];
-                        TAP.setString("sessionId", e.meta.session_id);
-                        TAP.setString("user", JSON.stringify(user));
-                        _callback && _callback({
-                            success : true,
-                            model : new model(user)
-                        });
-                    } else {
-                        Ti.API.error(e);
-                        _callback && _callback({
-                            success : false,
-                            model : null,
-                            error : e
-                        });
-                    }
-                });
-            }
-        });
-        // end extend
+				if (TAP.hasProperty("sessionId")) {
+					Ti.API.info("SESSION ID " + TAP.getString("sessionId"));
+					cloud.sessionId = TAP.getString("sessionId");
+					return true;
+				}
+				return false;
+			},
+			showMe : function(_callback) {
+				var cloud = this.config.Cloud;
+				var TAP = Ti.App.Properties;
+				cloud.Users.showMe(function(e) {
+					if (e.success) {
+						var user = e.users[0];
+						TAP.setString("sessionId", e.meta.session_id);
+						TAP.setString("user", JSON.stringify(user));
+						_callback && _callback({
+							success : true,
+							model : new model(user)
+						});
+					} else {
+						Ti.API.error(e);
+						_callback && _callback({
+							success : false,
+							model : null,
+							error : e
+						});
+					}
+				});
+			},
+			updateFacebookLoginStatus : function(_accessToken, _opts) {
+				var cloud = this.config.Cloud;
+				var TAP = Ti.App.Properties;
 
-        return Model;
-    },
+				// if not logged into facebook, then exit function
+				if (Alloy.Globals.FB.loggedIn == false) {
+					_opts.error && _opts.error({
+						success : false,
+						model : null,
+						error : "Not Logged into Facebook"
+					});
+					alert('Please Log Into Facebook first');
+					return;
+				}
 
-    extendCollection : function(Collection) {
-        _.extend(Collection.prototype, {
+				// we have facebook  access token so we are good
+				cloud.SocialIntegrations.externalAccountLogin({
+					type : "facebook",
+					token : _accessToken
+				}, function(e) {
+					if (e.success) {
+						var user = e.users[0];
+						TAP.setString("sessionId", e.meta.session_id);
+						TAP.setString("user", JSON.stringify(user));
 
-            // extended functions go here
+						// save how we logged in
+						TAP.setString("loginType", "FACEBOOK");
 
-        });
-        // end extend
+						_opts.success && _opts.success({
+							success : true,
+							model : new model(user),
+							error : null
+						});
+					} else {
+						Ti.API.error(e);
+						_opts.error && _opts.error({
+							success : false,
+							model : null,
+							error : e
+						});
+					}
+				});
+			}
+		});
+		// end extend
 
-        return Collection;
-    },
+		return Model;
+	},
+
+	extendCollection : function(Collection) {
+		_.extend(Collection.prototype, {
+
+			// extended functions go here
+
+		});
+		// end extend
+
+		return Collection;
+	},
 };
