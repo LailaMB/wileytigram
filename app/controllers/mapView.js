885c4ae4580de3ab25edcaceffc7286cdd8c1a79
Chaim Krause <chaim@chaim.com>
2013-11-04 14:12:05 -0600

    Cannot upload photo. Get alert box with "Error saving photo undefined".

---
+++ app/controllers/mapView.js
@@ -0,0 +1,32 @@
+var args = arguments[0] || {};
+var MapModule = Ti.Map;
+
+// get the photo object from the parameters
+var coords = args.photo.get("custom_fields").coordinates[0];
+var locationString = args.photo.get("custom_fields").location_string;
+
+// create annotation
+var annotation = MapModule.createAnnotation({
+	latitude : Number(coords[1]),
+	longitude : Number(coords[0]),
+	title : args.photo.get("title"),
+	subtitle : locationString,
+	myid : args.photo.id
+	//leftView : imageView,
+	// animate : true
+});
+// set the header
+$.thumb.image = args.photo.get("urls")["preview"];
+$.title.text = args.photo.get("title");
+$.location.text = locationString;
+
+// add them to map
+$.mapview.setAnnotations([annotation]);
+
+// set the region around the photo
+$.mapview.setRegion({
+	latitude : annotation.latitude,
+	longitude : annotation.longitude,
+	latitudeDelta : 0.040,
+	longitudeDelta : 0.040
+});
---
+++ app/lib/geo.js
@@ -0,0 +1,52 @@
+function reverseGeocoder(_lat, _lng, _callback) {
+    var title;
+
+    Ti.Geolocation.purpose = "Wiley Alloy App Demo";
+
+    // callback method converting lat lng into a location/address
+    Ti.Geolocation.reverseGeocoder(_lat, _lng, function(_data) {
+        if (_data.success) {
+            var place = _data.places[0];
+            if (place.city === "") {
+                title = place.address;
+            } else {
+                title = place.street + " " + place.city;
+            }
+        } else {
+            title = "No Address Found: " + _lat + ", " + _lng;
+        }
+        _callback(title);
+    });
+}
+
+exports.getCurrentLocation = function(_callback) {
+    if (!Ti.Geolocation.getLocationServicesEnabled()) {
+        alert('Location Services are not enabled');
+        _callback(null, 'Location Services are not enabled');
+        return;
+    }
+
+    Ti.Geolocation.purpose = "Get Current Location";
+    Ti.Geolocation.accuracy = Ti.Geolocation.ACCURACY_BEST;
+    Ti.Geolocation.distanceFilter = 10;
+    Ti.Geolocation.getCurrentPosition(function(_location) {
+        if (!_location.error && _location && _location.coords) {
+
+            var lat, lng;
+            Ti.API.info(JSON.stringify(_location));
+
+            lat = _location.coords.latitude;
+            lng = _location.coords.longitude;
+
+            reverseGeocoder(lat, lng, function(_title) {
+                _callback({
+                    coords : _location.coords,
+                    title : _title
+                }, null);
+            });
+        } else {
+            alert('Location Services an Error');
+            _callback(null, _location.error);
+        }
+    });
+};
