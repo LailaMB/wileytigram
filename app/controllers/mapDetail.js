a6a9d3916b3b5df4a530be87e749d2daea46c6f4
Aaron Saunders <aaron@clearlyinnovative.com>
2014-03-02 21:29:16 -0500

    appropriately setting the window titles for android actionBar - THIS REQUIRES EDITING OLD CHAPTERS

--- app/controllers/mapDetail.js
+++ app/controllers/mapDetail.js
@@ -12,33 +12,33 @@ var count = currentPhoto.attributes.reviews_count !== undefined ? currentPhoto.a
 // modify the button title to show the comment count
 // if there are comments already associated to photo
 if (count !== 0) {
-	$.commentButton.title = "Comments (" + count + ")";
+    $.commentButton.title = "Comments (" + count + ")";
 }
 
 $.buttonContainer.addEventListener('click', function(_event) {
-	// add the model information as data to event
-	_event.data = currentPhoto;
-	parameters.clickHandler(_event);
+    // add the model information as data to event
+    _event.data = currentPhoto;
+    parameters.clickHandler(_event);
 });
 
 $.getView().addEventListener("androidback", androidBackEventHandler);
 
 function androidBackEventHandler(_event) {
-	_event.cancelBubble = true;
-	_event.bubbles = false;
-	$.getView().removeEventListener("androidback", androidBackEventHandler);
-	$.getView().close();
+    _event.cancelBubble = true;
+    _event.bubbles = false;
+    $.getView().removeEventListener("androidback", androidBackEventHandler);
+    $.getView().close();
 }
 
 // Setup the menus and actionBar for Android if necessary
 $.getView().addEventListener("open", function() {
-	OS_ANDROID && ($.getView().activity.onCreateOptionsMenu = function() {
-		var actionBar = $.getView().activity.actionBar;
-		if (actionBar) {
-			actionBar.displayHomeAsUp = true; title :
-			"Photo Detail", actionBar.onHomeIconItemSelected = function() {
-				$.getView().close();
-			};
-		}
-	});
+    OS_ANDROID && ($.getView().activity.onCreateOptionsMenu = function() {
+        var actionBar = $.getView().activity.actionBar;
+        if (actionBar) {
+            actionBar.displayHomeAsUp = true;
+            actionBar.onHomeIconItemSelected = function() {
+                $.getView().close();
+            };
+        }
+    });
 });
--- app/controllers/mapView.js
+++ app/controllers/mapView.js
@@ -30,3 +30,25 @@ $.mapview.setRegion({
     latitudeDelta : 0.040,
     longitudeDelta : 0.040
 });
+
+$.getView().addEventListener("androidback", androidBackEventHandler);
+
+function androidBackEventHandler(_event) {
+    _event.cancelBubble = true;
+    _event.bubbles = false;
+    $.getView().removeEventListener("androidback", androidBackEventHandler);
+    $.getView().close();
+}
+
+// Setup the menus and actionBar for Android if necessary
+$.getView().addEventListener("open", function() {
+    OS_ANDROID && ($.getView().activity.onCreateOptionsMenu = function() {
+        var actionBar = $.getView().activity.actionBar;
+        if (actionBar) {
+            actionBar.displayHomeAsUp = true;
+            actionBar.onHomeIconItemSelected = function() {
+                $.getView().close();
+            };
+        }
+    });
+});
